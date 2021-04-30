15d0478aa46d50765e57794ba28dc0b36e6fe3e1
peter greaves <peter.greaves64@gmail.com>
2021-04-30 15:17:25 +0100

    fixed connection to docker mongo with app props

--- src/main/java/com/springfirst/learning/rest/spring5webfluxrest/bootstrap/Bootstrap.java
+++ src/main/java/com/springfirst/learning/rest/spring5webfluxrest/bootstrap/Bootstrap.java
@@ -1,10 +1,16 @@
 package com.springfirst.learning.rest.spring5webfluxrest.bootstrap;
 
+import com.springfirst.learning.rest.spring5webfluxrest.domain.Category;
 import com.springfirst.learning.rest.spring5webfluxrest.domain.Vendor;
+import com.springfirst.learning.rest.spring5webfluxrest.repositories.CategoryRepository;
 import com.springfirst.learning.rest.spring5webfluxrest.repositories.VendorRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Component
@@ -12,9 +18,11 @@ import java.util.UUID;
 public class Bootstrap implements CommandLineRunner {
 
     private final VendorRepository vendorRepository;
+    private final CategoryRepository categoryRepository;
 
-    public Bootstrap(VendorRepository vendorRepository) {
+    public Bootstrap(VendorRepository vendorRepository, CategoryRepository categoryRepository) {
         this.vendorRepository = vendorRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Override
@@ -23,6 +31,7 @@ public class Bootstrap implements CommandLineRunner {
          vendorRepository.findAll().count().subscribe(k ->{
             if (k == 0){
                 loadVendors();
+                loadCategories();
             }
             else{
                 log.info("{} Vendors already present in database.", k);
@@ -31,16 +40,33 @@ public class Bootstrap implements CommandLineRunner {
 
     }
 
+
+    private void loadCategories(){
+
+        Category fruits = Category.builder().description("Fruits").build();
+        Category nuts = Category.builder().description("Nuts").build();
+        Category dried = Category.builder().description("Dried").build();
+        Category exotic = Category.builder().description("Exotic").build();
+        Category fresh = Category.builder().description("Fresh").build();
+        List<Category> cats = new ArrayList<>();
+        cats.add(fruits);
+        cats.add(nuts);
+        cats.add(exotic);
+        cats.add(dried);
+        cats.add(fresh);
+        categoryRepository.saveAll(cats).subscribe(c ->log.info("Cat created {}", c.getId()));
+    }
     private void loadVendors(){
 
-        String id = UUID.randomUUID().toString();
+        Vendor vendor1 = Vendor.builder().firstName("WH").lastName("Smiths").build();
+        Vendor vendor2 = Vendor.builder().firstName("Tim").lastName("Williams").build();
 
-        Vendor vendor1 = Vendor.builder().id(id).firstName("WH").lastName("Smiths").build();
-        Vendor vendor2 = Vendor.builder().id(id).firstName("Tim").lastName("Williams").build();
-        vendorRepository.save(vendor1);
-        vendorRepository.save(vendor2);
+        List<Vendor> vendors = new ArrayList<>();
+        vendors.add(vendor1);
+        vendors.add(vendor2);
+        vendorRepository.saveAll(vendors).subscribe(v ->log.info("Vendor created {}", v.getId()));
 
-        log.info("Vendors loaded.");
+
 
 
     }
--- src/main/java/com/springfirst/learning/rest/spring5webfluxrest/domain/Category.java
+++ src/main/java/com/springfirst/learning/rest/spring5webfluxrest/domain/Category.java
@@ -1,11 +1,13 @@
 package com.springfirst.learning.rest.spring5webfluxrest.domain;
 
+import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
+@Builder
 public class Category {
 
     @Id
